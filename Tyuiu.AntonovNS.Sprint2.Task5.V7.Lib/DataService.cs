af7bc24d45bb3a587e75d1ac7f67e1f141959b4a
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-10-15 21:00:30 +0500

    Task5

---
+++ Tyuiu.AntonovNS.Sprint2.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,52 @@
+﻿using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AntonovNS.Sprint2.Task5.V7.Lib
+{
+    public class DataService : ISprint2Task5V7
+    {
+        public string FindMonthName(int startYear, int n)
+        {
+            switch (n)
+            {
+                case 1:
+                    return "Январь";
+                break;
+                case 2:
+                    return "Февраль";
+                break;
+                case 3:
+                    return "Март";
+                break;
+                case 4:
+                    return "Апрель";
+                break;
+                case 5:
+                    return "Май";
+                break;
+                case 6:
+                    return "Июнь";
+                break;
+                case 7:
+                    return "Июль";
+                break;
+                case 8:
+                    return "Август";
+                break;
+                case 9:
+                    return "Сентябрь";
+                break;
+                case 10:
+                    return "Октябрь";
+                break;
+                case 11:
+                    return "Ноябрь";
+                break;
+                case 12:
+                    return "Декабрь";
+                break;
+                default:
+                    throw new ArgumentException($"Месяц должен быть от 1 до 12. значние {n}");
+            }
+        }
+    }
+}
---
+++ Tyuiu.AntonovNS.Sprint2.Task5.V7/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AntonovNS.Sprint2.Task5.V7.Lib;
+namespace Tyuiu.AntonovNS.Sprint2.Task5.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Антонов Н.С. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("#* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Антонов Никита Сергеевич |  ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("* С начала 1990 года по некоторый день прошло n месяцев и 2 дня.          *");
+            Console.WriteLine("* Определить название месяца (январь, февраль и т. п.) этого дня.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int n;
+            Console.WriteLine("Введите значние n: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.FindMonthName(1990,n));
+        }
+    }
+}
