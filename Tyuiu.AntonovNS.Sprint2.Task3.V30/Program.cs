39bc9fbe51b2f94ae8140a3f9f4fde01b4fa718d
Никита Антонов <Tyuiu.AntonovNS@outlook.com>
2024-10-15 18:31:08 +0500

    Task3

---
+++ Tyuiu.AntonovNS.Sprint2.Task3.V30/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.AntonovNS.Sprint2.Task3.V30.Lib;
+namespace Tyuiu.AntonovNS.Sprint2.Task3.V300
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Антонов Н.С. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("#* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Антонов Никита Сергеевич |  ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Условие:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь           *");
+            Console.WriteLine("* вводит значение переменной X с клавиатуры.                              *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой;             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Введите значние X: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x));
+        }
+    }
+}
